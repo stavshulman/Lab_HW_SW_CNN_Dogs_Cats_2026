c5f1d132f43b696523ba137c8966137f7a8e499c
stavshulman <stav.shulman@epfl.ch>
2026-03-18 17:25:12 +0100

    Task 2 started ! Cached coeffs. To test on HW and double check pragmas

---
+++ Task2_CNN_CACHE_FILTER_COEFF/src/conv2d.cpp
@@ -0,0 +1,71 @@
+//My contribution:
+#include "conv2d.hpp"
+
+inline TFXP FXP_Mult(TFXP a, TFXP b, uint32_t decimalBits = DECIMALS)
+{
+    //return a*b;
+    // We need a wider data type to correctly capture the result of the multiplication.
+    FXP_MULT res = (TFXP_MULT)a * (TFXP_MULT)b;
+    res = res >> decimalBits;
+    return res;
+}
+
+void Conv2D_HW(TFXP *input, TFXP * output, TFXP * coeffs,
+      uint32_t numChannels, uint32_t numFilters,
+      uint32_t inputWidth, uint32_t inputHeight,
+      uint32_t convWidth, uint32_t convHeight)
+{
+    // My contribution: preprocessor pragrams:
+    // We have access to 2 AXI4 buses, we only use one here for now.
+    #pragma HLS INTERFACE m_axi port=input offset=slave bundle=gmem0
+    #pragma HLS INTERFACE m_axi port=output offset=slave bundle=gmem0
+    #pragma HLS INTERFACE m_axi port=coeffs offset=slave bundle=gmem1
+
+    #pragma HLS INTERFACE s_axilite port=numFilters
+    #pragma HLS INTERFACE s_axilite port=numChannels
+    #pragma HLS INTERFACE s_axilite port=inputWidth
+    #pragma HLS INTERFACE s_axilite port=inputHeight
+    #pragma HLS INTERFACE s_axilite port=convWidth
+    #pragma HLS INTERFACE s_axilite port=convHeight
+    #pragma HLS INTERFACE s_axilite port=return
+
+    TFXP filterCoeffs[MAX_CHANNELS][MAX_CONV_H][MAX_CONV_W];
+    // Add array partitioning pragmas here
+    #pragma HLS ARRAY_PARTITION variable=filterCoeffs complete dim=2
+    #pragma HLS ARRAY_PARTITION variable=filterCoeffs complete dim=3
+
+    loop_load_filters : for (uint32_t iFilter = 0; iFilter < numFilters; ++ iFilter) {
+        loop_load_channels : for (uint32_t iChannel = 0; iChannel < numChannels; ++ iChannel) {
+            loop_load_coef_y : for (uint32_t y = 0; y < convHeight; ++y) {
+                loop_load_coef_x : for (uint32_t x = 0; x < convWidth; ++ x) {
+                    #pragma HLS PIPELINE II=1
+                    filterCoeffs[iChannel][y][x] = *(coeffs
+                                                    +iFilter*numChannels*convHeight*convWidth
+                                                    +iChannel*convHeight*convWidth
+                                                    +y*convWidth
+                                                    +x);
+                }
+            }
+        }
+    }
+
+    loop_convolve_y : for (uint32_t y = 0; y < (inputHeight-2); ++y) {
+        loop_convolve_x : for (uint32_t x = 0; x < (inputWidth-2); ++ x) {
+            TFXP acc = 0;
+
+            loop_acc_channels : for (uint32_t iChannel = 0; iChannel < numChannels; ++ iChannel) {
+                loop_acc_y : for (uint32_t cy = 0; cy < convHeight; ++ cy) {
+                    loop_acc_x : for (uint32_t cx = 0; cx < convWidth; ++cx) {
+                        #pragma HLS PIPELINE II=1
+                        TFXP pixelValue, coeffValue;
+                        coeffValue = filterCoeffs[iChannel][cy][cx];
+                        pixelValue = *(input + iChannel*inputWidth*inputHeight + (y+cy)*inputWidth + (x+cx));
+                        acc += FXP_Mult(coeffValue, pixelValue, DECIMALS);
+                    }
+                }
+            }
+            //output[iFilter][y][x] = acc;
+            *(output + iFilter * (inputHeight-2)*(inputWidth-2) + y*(inputWidth-2) + x) = acc;
+        }
+    }
+}
--- Task2_CNN_CACHE_FILTER_COEFF/src/conv2d.hpp
+++ Task2_CNN_CACHE_FILTER_COEFF/src/conv2d.hpp
@@ -9,6 +9,11 @@ const uint32_t DECIMALS = 20;
 typedef int32_t TFXP;     // Parameters and activations
 typedef int64_t TFXP_MULT;// Intermmediate results of multiplications
 
+// Max values for pragmas
+const uint32_t MAX_CHANNELS = 256;
+const uint32_t MAX_CONV_H   = 3;
+const uint32_t MAX_CONV_W   = 3;
+
 // Check if convHeight = 3 is correst in header/cpp file
 void Conv2D_HW(TFXP *input, TFXP * output, TFXP * coeffs,
       uint32_t numChannels, uint32_t numFilters,
@@ -16,4 +21,3 @@ void Conv2D_HW(TFXP *input, TFXP * output, TFXP * coeffs,
       uint32_t convWidth = 3, uint32_t convHeight = 3);
 
 #endif // CONV2D_H
-
