786bd6af8369aab7252ccada15130d0dd1a3bccc
stavshulman <stav.shulman@epfl.ch>
2026-03-26 12:20:49 +0100

    Task3 works on PYNQ, 81 seconds ???? to debug

--- Task3/src/conv2d.cpp
+++ Task3/src/conv2d.cpp
@@ -36,43 +36,39 @@ void Conv2D_HW(TFXP *input, TFXP *output, TFXP *coeffs,
     #pragma HLS ARRAY_PARTITION variable=filterCoeffs complete dim=2
     #pragma HLS ARRAY_PARTITION variable=filterCoeffs complete dim=3
 
-    // Maybe change complete -> cyclic
-    #pragma HLS ARRAY_PARTITION variable=lineBuffer0 cyclic factor=4
-    #pragma HLS ARRAY_PARTITION variable=lineBuffer1 cyclic factor=4
-    #pragma HLS ARRAY_PARTITION variable=lineBuffer2 cyclic factor=4
+    // Maybe change complete -> cyclic, maybe block (?)
+    #pragma HLS ARRAY_PARTITION variable=lineBuffer0 cyclic factor=3
+    #pragma HLS ARRAY_PARTITION variable=lineBuffer1 cyclic factor=3
+    #pragma HLS ARRAY_PARTITION variable=lineBuffer2 cyclic factor=3
 
     loop_filters: for(uint32_t iFilter = 0; iFilter < numFilters; ++iFilter) {
         // Need to add TRICOUNT for Vitis HLS sim
         #pragma HLS LOOP_TRIPCOUNT min=1 max=MAX_NUM_FILTERS
 
-        // Load buffers
-        load_channels_buff: for(uint32_t iChannel = 0; iChannel < numChannels; ++iChannel) {
+        loop_channels_main: for(uint32_t iChannel = 0; iChannel < numChannels; ++iChannel) {
             #pragma HLS LOOP_TRIPCOUNT min=1 max=MAX_CHANNELS
 
+            // Load buffers
             load_cols_buff: for(uint32_t cols = 0; cols < inputWidth; ++cols) {
                 #pragma HLS LOOP_TRIPCOUNT min=1 max=MAX_INPUT_W
                 #pragma HLS PIPELINE II=1
 
                 // loading rows -> lineBuffer
-                lineBuffer0[iChannel*inputWidth + cols] = *(input
-                                                        + iChannel*inputWidth*inputHeight
-                                                        + 0*inputWidth
-                                                        + cols);
-                lineBuffer1[iChannel*inputWidth + cols] = *(input
-                                                        + iChannel*inputWidth*inputHeight
-                                                        + 1*inputWidth
-                                                        + cols);
-                lineBuffer2[iChannel*inputWidth + cols] = *(input
-                                                        + iChannel*inputWidth*inputHeight
-                                                        + 2*inputWidth
-                                                        + cols);
+                lineBuffer0[cols] = *(input
+                                    + iChannel*inputWidth*inputHeight
+                                    + 0*inputWidth
+                                    + cols);
+                lineBuffer1[cols] = *(input
+                                    + iChannel*inputWidth*inputHeight
+                                    + 1*inputWidth
+                                    + cols);
+                lineBuffer2[cols] = *(input
+                                    + iChannel*inputWidth*inputHeight
+                                    + 2*inputWidth
+                                    + cols);
             }
-        } // done loading line buffers
 
-        // loading filter coeffs for caching (task 2)
-        load_channels_filt: for(uint32_t iChannel = 0; iChannel < numChannels; ++iChannel) {
-            #pragma HLS LOOP_TRIPCOUNT min=1 max=MAX_CHANNELS
-
+            // loading filter coeffs (task 2)
             load_rows_filt: for(uint32_t y = 0; y < convHeight; ++y) {
                 // convWidth always 3
                 #pragma HLS LOOP_TRIPCOUNT min=MAX_CONV_H max=MAX_CONV_H
@@ -88,51 +84,49 @@ void Conv2D_HW(TFXP *input, TFXP *output, TFXP *coeffs,
                                                         + y*convWidth
                                                         + x);
                 }
-            }
-        } // done loading filter coeffs, matches task2
+            } // done loading filter coeffs
 
-        // beginning of convolution (finally)
-        loop_height: for(uint32_t y = 0; y < (inputHeight-2); ++y) {
-            // max = 256-2
-            #pragma HLS LOOP_TRIPCOUNT min=1 max=254
-
-            loop_width: for(uint32_t x = 0; x < (inputWidth-2); ++x) {
+            // beginning of convolution (yayyyyy)
+            loop_height: for(uint32_t y = 0; y < (inputHeight-2); ++y) {
+                // max = 256-2
                 #pragma HLS LOOP_TRIPCOUNT min=1 max=254
+                #pragma HLS PIPELINE II=1
 
-                TFXP acc = 0;
+                loop_width: for(uint32_t x = 0; x < (inputWidth-2); ++x) {
+                    #pragma HLS LOOP_TRIPCOUNT min=1 max=254
 
-                loop_channel: for(uint32_t iChannel = 0; iChannel < numChannels; ++iChannel) {
-                    #pragma HLS LOOP_TRIPCOUNT min=1 max=MAX_CHANNELS
+                    TFXP acc = 0;
 
                     loop_acc: for(uint32_t cx = 0; cx < convWidth; ++cx) {
                         #pragma HLS LOOP_TRIPCOUNT min=MAX_CONV_W max=MAX_CONV_W
-                        #pragma HLS PIPELINE II=1
+                        // #pragma HLS PIPELINE II=1 <- cannot do that 
+                        #pragma HLS UNROLL
 
-                        acc += FXP_Mult(filterCoeffs[iChannel][0][cx], lineBuffer0[iChannel*inputWidth + x + cx])
-                            + FXP_Mult(filterCoeffs[iChannel][1][cx], lineBuffer1[iChannel*inputWidth + x + cx])
-                            + FXP_Mult(filterCoeffs[iChannel][2][cx], lineBuffer2[iChannel*inputWidth + x + cx]);
+                        acc += FXP_Mult(filterCoeffs[iChannel][0][cx], lineBuffer0[x + cx])
+                            + FXP_Mult(filterCoeffs[iChannel][1][cx], lineBuffer1[x + cx])
+                            + FXP_Mult(filterCoeffs[iChannel][2][cx], lineBuffer2[x + cx]);
                     }
-                } // done looping over the numChannels
 
-                *(output + iFilter*(inputHeight-2)*(inputWidth-2) + y*(inputWidth-2) + x) = acc;
-            }
+                    if(iChannel == 0) {
+                        *(output + iFilter*(inputHeight-2)*(inputWidth-2) + y*(inputWidth-2) + x) = acc;
+                    } else {
+                        *(output + iFilter*(inputHeight-2)*(inputWidth-2) + y*(inputWidth-2) + x) += acc;
+                    }
+                }
 
-            // update lineBuffers
-            loop_width_update: for(uint32_t x = 0; x < inputWidth; ++x) {
-                #pragma HLS LOOP_TRIPCOUNT min=1 max=MAX_INPUT_W
-                #pragma HLS PIPELINE II=1
+                // lineBuffers
+                loop_width_update: for(uint32_t x = 0; x < inputWidth; ++x) {
+                    #pragma HLS LOOP_TRIPCOUNT min=1 max=MAX_INPUT_W
+                    #pragma HLS PIPELINE II=1
 
-                loop_channel_update: for(uint32_t iChannel = 0; iChannel < numChannels; ++iChannel) {
-                    #pragma HLS LOOP_TRIPCOUNT min=1 max=MAX_CHANNELS
+                    lineBuffer0[x] = lineBuffer1[x];
+                    lineBuffer1[x] = lineBuffer2[x];
 
-                    lineBuffer0[iChannel*inputWidth + x] = lineBuffer1[iChannel*inputWidth + x];
-                    lineBuffer1[iChannel*inputWidth + x] = lineBuffer2[iChannel*inputWidth + x];
-
-                    if(y <= (inputHeight-2)) {
-                        lineBuffer2[iChannel*inputWidth + x] = *(input
-                                                            + iChannel*inputWidth*inputHeight
-                                                            + (y+3)*inputWidth
-                                                            + x);
+                    if((y+3) < inputHeight) {
+                        lineBuffer2[x] = *(input
+                                        + iChannel*inputWidth*inputHeight
+                                        + (y+3)*inputWidth
+                                        + x);
                     }
                 }
             }
